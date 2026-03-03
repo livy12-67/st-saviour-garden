f6317ab78d888a6c61be31e4e3d76ed94cd8683f
ocrammer <ocrammer28@stsaviour.org>
2026-03-03 14:06:23 -0500

    mcdonalds

--- garden.py
+++ garden.py
@@ -1,6 +1,54 @@
 
-
 if __name__ == '__main__':
+    print('solid')
+    for i in range(11): 
+        for j in range(11):
+             print('🌷', end='')
+        print()
 
-    # print without a line break using this function
-    print('🌷', end='')
+    print('horizontal')
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0:
+                print('🌷', end='')
+            else:
+                print('🌱', end='')
+        print('')
+
+    print('vertical')
+    for i in range(11):
+        for j in range(11):
+            if j % 2 == 0:
+                 print('🌷', end='')
+            else:
+                 print('🌱', end='')
+        print('')
+
+    print('diagnol')
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0 and j % 2 == 0:
+                print('🌷', end='')
+            elif i % 2 == 1 and j % 2 == 1:
+                print('🌷', end='')
+            else:
+                print('🌱', end='')     
+        print('')
+
+    print('plaid')
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0 or j % 2 == 0:
+                print('🌷', end='')   
+            else:
+                print('🌱', end='')  
+        print('') 
+
+    print('bonus')
+    for i in range(11):
+        for j in range(11):
+            if i == 5 or j == 5:
+                print('🌷', end='')  
+            else:
+                print('🌱', end='')  
+        print('')
